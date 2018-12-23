b343c41b6be853177b91ea96f05836b14e1439a0
rbs2rbs <renan.bisposilva@gmail.com>
2018-12-23 17:54:30 -0300

    deploy

--- conciliare/settings.py
+++ conciliare/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'i#723dw3ya@gsvkv(&5@vds1*j60d$4p(rd8ivh6ey=$*a#jr#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'paginas',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'America/Sao_Paulo'
+LANGUAGE_CODE = 'pt-BR'
 
-TIME_ZONE = 'pt-BR'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
